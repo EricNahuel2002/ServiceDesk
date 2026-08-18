6bdc7e79b2debff01953fde8e457c35b27507c9f
EricNahuel2002 <ericaquino2002@gmail.com>
2026-08-18 19:10:55 -0300

    chore: remove orphaned Priority catalog entity
    Priority catalog has been replaced by the global TicketPriority enum.
    The entity is no longer referenced by any active code.

--- src/ServiceDesk.Infrastructure/DependencyInjection.cs
+++ src/ServiceDesk.Infrastructure/DependencyInjection.cs
@@ -93,6 +93,7 @@ public static class DependencyInjection
         services.AddScoped<ICompanyRepository, CompanyRepository>();
         services.AddScoped<IChatMessageRepository, ChatMessageRepository>();
         services.AddScoped<ISlaRepository, SlaRepository>();
+        services.AddScoped<IBusinessHoursCalculator, BusinessHoursCalculator>();
         services.AddHttpContextAccessor();
         services.AddScoped<ICurrentUserService, CurrentUserService>();
         services.AddScoped<ServiceDeskDbInitializer>();
